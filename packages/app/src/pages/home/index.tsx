3df30482dd447c1bfec93b1611bd0f6ef0073ba0
Sooyoung <dannylisa67@gmail.com>
2022-02-06 17:49:02 +0900

    Update index.tsx

--- packages/app/src/pages/home/index.tsx
+++ packages/app/src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import { Text } from "materials";
-import { MdNotInterested } from "react-icons/md";
+import { ImHome } from "react-icons/im";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -10,11 +10,10 @@ const Wrapper = styled.div`
     height: 70vh;
 `
 
-export const Home = () => {
-    
+export const Home = () => {    
     return (
         <Wrapper>
-            <MdNotInterested color="red" size={84} />
+            <ImHome color="#2c60e5" size={84} />
             <Text type="H2" size={32} content="Henesis Frontend" marginTop={24} />
             <Text type="D1" size={16} content="지갑을 등록해 주세요." marginTop={12} marginBottom={24}  />
         </Wrapper>
